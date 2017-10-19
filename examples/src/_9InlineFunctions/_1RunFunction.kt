444c048d858294debd4003725d202e5fbcafa6b8
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2017-10-19 15:57:37 +0300

    Added examples for run, takeIf, takeUnless, repeat functions

--- examples/src/_9InlineFunctions/_1RunFunction.kt
+++ examples/src/_9InlineFunctions/_1RunFunction.kt
@@ -1,34 +1,10 @@
 package _9InlineFunctions
 
-class Board(val width: Int) {
-    val indices1: List<Pair<Int, Int>>
-
-    init {
-        val result = mutableListOf<Pair<Int, Int>>()
-        for (i in 1..width) {
-            for (j in 1..width) {
-                result += i to j
-            }
-        }
-        indices1 = result
-    }
-
-    val indices = run {
-        val result = mutableListOf<Pair<Int, Int>>()
-        for (i in 1..width) {
-            for (j in 1..width) {
-                result += i to j
-            }
-        }
-        result.toList()
-    }
+val foo = run {
+    println("Calculating foo...")
+    "foo"
 }
 
-inline fun <R> run(block: () -> R): R = block()
-
 fun main(args: Array<String>) {
-    val table = Board(3)
-    for ((i, j) in table.indices) {
-        print("($i, $j) ")
-    }
+    println(foo)
 }
---
+++ examples/src/_9InlineFunctions/_4RepeatFunction.kt
@@ -0,0 +1,7 @@
+package _9InlineFunctions
+
+fun main(args: Array<String>) {
+    repeat(10) {
+        println("Welcome!")
+    }
+}
